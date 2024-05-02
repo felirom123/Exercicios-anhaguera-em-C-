12f9065d00896d01e07a0d65e8db5bd2b2f0cc05
FELIPE BASILIO ROMEU <165338521+felirom123@users.noreply.github.com>
2024-05-02 20:21:56 -0300

    Create Vetor_FOR.c

---
+++ Vetor_FOR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()  {
+ int list[5] = {1, 2, 3, 4, 5};
+ int scan;
+    
+    for (scan = 0; scan <= 4; scan++ )  {
+      printf("%i \n", list[scan]);/* a variavel de controle do for 'scan' dentro do 'list' é para atribuir a posição do valor dentro da 'list' */  
+    }/* int list[5] = {1(posição 0), 2(posição 1), 3(posição 2), 4(posição 3), 5(posição 4)}; */
+    
+return 0;
+}
